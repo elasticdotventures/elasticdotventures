34360764a4d26c7b3e9782b94e8ff93c219b759c
b <b@elastic.ventures>
2022-05-31 19:07:37 +1000

    modified:   git-friendly-fix.sh

--- git-friendly-fix.sh
+++ git-friendly-fix.sh
@@ -1,6 +1,15 @@
 
 
-gh repo fork containers/shortnames --fork-name containers-shortnames
+gh repo fork containers/shortnames --fork-name containers-shortnames --clone
 cd containers-shortnames
 git checkout -b chore-matrix
 gh pr create 
+
+#nektos_act:gitter.im
+ # [![Chat on Matrix](https://matrix.to/img/matrix-badge.svg)](https://matrix.to/#/#nektos_act:gitter.im) 
+# [![Chat on Matrix](https://matrix.to/img/matrix-badge.svg)](https://matrix.to/#/#containers:libera.chat)
+
+
+# https://gist.github.com/Chaser324/ce0505fbed06b947d962
+
+
